e91640d93147cd40282fcbd30456defc0dd2027d
samadhi-k <77055729+samadhi-k@users.noreply.github.com>
2022-11-14 13:56:54 +0530

    Added charts

---
+++ client/src/components/AreaChart.js
@@ -0,0 +1,17 @@
+import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
+
+const AreasChart = ({data}) => {
+  return (
+    <ResponsiveContainer width='100%' height={300}>
+        <AreaChart data={data} margin={{top:50}}>
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='date'/>
+            <YAxis allowDecimals={false}/>
+            <Tooltip/>
+            <Area type='monotone' dataKey='count' stroke='#2cb1bc' fill='#bef8fd' />
+        </AreaChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default AreasChart
--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Wrapper from '../assets/wrappers/ChartsContainer.js'
+import { useAppContext } from '../context/appContext'
+import AreasChart from './AreaChart'
+import BarsChart from './BarChart'
+
 
 const ChartsContainer = () => {
+
+  const [barChart, setBarChart] = useState(true)
+  const  {monthlyApplications:data} = useAppContext()
   return (
-    <div>ChartsContainer</div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button 
+        type='button' 
+        onClick={() => {
+          setBarChart(!barChart)
+        }}>
+          {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+
+      { barChart ? <BarsChart data={data}/> :
+      <AreasChart data={data}/>}
+    </Wrapper>
   )
 }
 
